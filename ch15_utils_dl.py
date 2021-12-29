5ae52d92bc0bbecd2b5fd98e0818b1badb0fcc15
A.J <ajhergenroeder@icloudl.com>
2021-12-29 17:01:32 -0500

    ch 15: dl and nondl portions separated

--- ch15_utils_dl.py
+++ ch15_utils_dl.py
@@ -23,24 +23,10 @@ from params import \
 np.random.seed(42)
 tf.random.set_seed(42)
 
+
 # # # # # # # # # #
 ### Time series data
 # # # # # # # # # #
-
-def generate_time_series(batch_size, n_steps):
-    """
-    Generate dataset
-
-    :param batch_size:
-    :param n_steps:
-    :return:
-    """
-    freq1, freq2, offsets1, offsets2 = np.random.rand(4, batch_size, 1)
-    time = np.linspace(0, 1, n_steps)
-    series = 0.5 * np.sin((time - offsets1) * (freq1 * 10 + 10))  #   wave 1
-    series += 0.2 * np.sin((time - offsets2) * (freq2 * 20 + 20)) # + wave 2
-    series += 0.1 * (np.random.rand(batch_size, n_steps) - 0.5)   # + noise
-    return series[..., np.newaxis].astype(np.float32)
 
 def last_time_step_mse(Y_true, Y_pred):
     return keras.metrics.mean_squared_error(Y_true[:, -1], Y_pred[:, -1])
@@ -112,50 +98,3 @@ def wavenet_residual_block(inputs, n_filters, dilation_rate):
     z = GatedActivationUnit()(z)
     z = keras.layers.Conv1D(n_filters, kernel_size=1)(z)
     return keras.layers.Add()([z, inputs]), z
-
-# # # # # # #
-### Plotting
-# # # # # # #
-def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
-    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
-    print("Saving figure", fig_id)
-    if tight_layout:
-        plt.tight_layout()
-    plt.savefig(path, format=fig_extension, dpi=resolution)
-
-
-
-def plot_series(series, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$", legend=True):
-    plt.plot(series, ".-")
-    if y is not None:
-        plt.plot(n_steps, y, "bo", label="Target")
-    if y_pred is not None:
-        plt.plot(n_steps, y_pred, "rx", markersize=10, label="Prediction")
-    plt.grid(True)
-    if x_label:
-        plt.xlabel(x_label, fontsize=16)
-    if y_label:
-        plt.ylabel(y_label, fontsize=16, rotation=0)
-    plt.hlines(0, 0, 100, linewidth=1)
-    plt.axis([0, n_steps + 1, -1, 1])
-    if legend and (y or y_pred):
-        plt.legend(fontsize=14, loc="upper left")
-
-def plot_learning_curves(loss, val_loss):
-    plt.plot(np.arange(len(loss)) + 0.5, loss, "b.-", label="Training loss")
-    plt.plot(np.arange(len(val_loss)) + 1, val_loss, "r.-", label="Validation loss")
-    plt.gca().xaxis.set_major_locator(mpl.ticker.MaxNLocator(integer=True))
-    plt.axis([1, 20, 0, 0.05])
-    plt.legend(fontsize=14)
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    plt.grid(True)
-
-def plot_multiple_forecasts(X, Y, Y_pred):
-    n_steps = X.shape[1]
-    ahead = Y.shape[1]
-    plot_series(X[0, :, 0])
-    plt.plot(np.arange(n_steps, n_steps + ahead), Y[0, :, 0], "bo-", label="Actual")
-    plt.plot(np.arange(n_steps, n_steps + ahead), Y_pred[0, :, 0], "rx-", label="Forecast", markersize=10)
-    plt.axis([0, n_steps + ahead, -1, 1])
-    plt.legend(fontsize=14)
